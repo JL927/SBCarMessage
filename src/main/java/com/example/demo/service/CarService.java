5cf3f0e58cc959b50ddea1a077221371dd708e69
WQ <3332140967@qq.com>
2021-11-02 14:16:33 +0800

    11.2 fifth commit

--- src/main/java/com/example/demo/service/CarService.java
+++ src/main/java/com/example/demo/service/CarService.java
@@ -6,7 +6,11 @@ import com.example.demo.mapper.CityMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.Collator;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 @Service
 public class CarService {//处理业务逻辑
 
@@ -17,10 +21,14 @@ public class CarService {//处理业务逻辑
 
 
     public List<Car> findAll(){
-        return carMapper.findAll();
+        List<Car> list=carMapper.findAll();
+        this.SortList(list);
+        return list;
     }
     public List<Car> findAllByCity(String name){
-        return carMapper.findAllByCity(cityService.getCityId(name));
+        List<Car> list=carMapper.findAllByCity(cityService.getCityId(name));
+        this.SortList(list);
+        return list;
     }
 
     public void changeViolation(int id,String vio){
@@ -29,6 +37,11 @@ public class CarService {//处理业务逻辑
 
     public Car findCar(int engineId) {
         return carMapper.findCar(engineId);
+    }
+    public Car findCarBS(int engineId){
+        List<Car> list=carMapper.findAll();
+        this.SortList(list);
+        return this.binarySearchCar(list,carMapper.findCar(engineId).getLicense());
     }
     public Car findCarByName(String name) {
         return carMapper.findCarByName(name);
@@ -45,5 +58,28 @@ public class CarService {//处理业务逻辑
                        String model)
     {
         carMapper.addCar(engine_id, license, driver_name, cityService.getCityId(city_name), violation, model);
+    }
+
+    public Car binarySearchCar(List<Car> list,String str){
+        int left = 0;
+        int right = list.size() - 1;
+        int mid ;
+        while (left <= right){
+            mid = (left + right)/2;
+            if(str.equals(list.get(mid).getLicense()))
+                return list.get(mid);
+            if(str.compareTo(list.get(mid).getLicense()) < 0)
+                right = mid - 1;
+            if(str.compareTo(list.get(mid).getLicense()) > 0)
+                left = mid + 1;
+        }
+        return null;
+    }
+    public void SortList(List<Car> list){
+        Collator co = Collator.getInstance(Locale.CHINA);
+        list.sort(co);
+//        for (Car car: list) {
+//            System.out.println(car.getLicense());
+//        }
     }
 }
